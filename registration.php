1eac62b4c8dbf5d5f8ca0096f41dfea9d2e2a0b1
gemng <gem@4d.com.hk>
2017-02-20 15:19:38 +0800

    First commit with Installation Instruction

---
+++ registration.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Magento 2 Chinese (Hong Kong) language pack
+ *
+ * @category   Language
+ * @package    zh_Hant_HK
+ * @license    http://www.opensource.org/licenses/mit-license.html  MIT License
+ * @author     Clarence Ho (http://www.skywidesoft.com/)
+ */
+
+\Magento\Framework\Component\ComponentRegistrar::register(
+    \Magento\Framework\Component\ComponentRegistrar::LANGUAGE,
+    'fd_zh_hant_hk',
+    __DIR__
+);
